a89d2324b9e373a08ed0b6df2a43db8826936954
parkhi <parkhi.1420@gmail.com>
2026-04-29 17:46:29 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       // vector<int> memo(nums.size(), -1);
+        //memo[0] = 1;
+        //return canjump(nums, nums.size()-1, memo);
+        int goal = nums.size()-1;
+
+        for(int i= nums.size()-1; i>=0; i--) {
+            if(nums[i]+i >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal==0;
+
+    }
+
+    bool canjump(vector<int>& nums, int ind, vector<int>& memo) {
+        //cout<<"ind is "<<ind<<endl;
+
+        if(memo[ind] != -1) {
+            return memo[ind];
+        }
+        for(int i=ind-1; i>=0; i--) {
+            //cout<<"nums[i] "<<nums[i] <<" ind-i "<<ind-i<<endl;
+            if((nums[i] >= (ind-i))) {
+                if( canjump(nums, i, memo)) {
+                    memo[i] = 1;
+                    return true;
+                }
+            }
+        }
+        memo[ind] = 0;
+        return false;
+    }
+};
