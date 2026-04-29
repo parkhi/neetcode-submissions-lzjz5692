48f68e94705694453c6b2f0499f2d7e6b3458e0d
parkhi <parkhi.1420@gmail.com>
2026-04-29 17:30:03 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        return canjump(nums, nums.size()-1);
+    }
+
+    bool canjump(vector<int>& nums, int ind) {
+        cout<<"ind is "<<ind<<endl;
+
+        if(ind == 0) {
+            return true;
+        }
+        for(int i=ind-1; i>=0; i--) {
+            cout<<"nums[i] "<<nums[i] <<" ind-i "<<ind-i<<endl;
+            if((nums[i] >= (ind-i))) {
+                if( canjump(nums, i)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
